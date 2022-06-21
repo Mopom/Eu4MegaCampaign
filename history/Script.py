357e0ebee9bb3b90d598ddc52a107cb19ce71fe8
Mopom <simmblas@gmail.com>
2022-06-21 10:05:41 +0200

    Added script to convert provinces, converted provinces.
    Script is dangerous, only use if you know what you're doing.

---
+++ history/Script.py
@@ -0,0 +1,27 @@
+import glob
+import os
+from pathlib import Path
+
+ids_list = list(range(1, 4940, 1))
+for ids in ids_list:
+    replaced_content = ""
+    filePath = 'provinces/' + str(ids) + ' -*.txt'
+    fileName = glob.glob(filePath)
+    if len(fileName) > 0:
+        file = open(fileName[0], "r")
+        print("Changing file: " + fileName[0])
+        for line in file:
+            line = line.strip()
+
+            if "religion = rhenism" in line:
+                new_line = "religion = samaritan_faith"
+            else:
+                new_line = line
+
+            replaced_content = replaced_content + new_line + "\n"
+
+        file.close()
+
+        write_file = open(fileName[0], "w")
+        write_file.write(replaced_content)
+        write_file.close()
