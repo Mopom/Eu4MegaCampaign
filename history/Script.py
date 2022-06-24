4c4664a194de4ec4974680a2c17ce960c58fe82a
Mopom <simmblas@gmail.com>
2022-06-24 12:25:46 +0200

    Exploded the Federation

--- history/Script.py
+++ history/Script.py
@@ -2,21 +2,23 @@ import glob
 import os
 from pathlib import Path
 
-ids_list = list(range(1, 4940, 1))
+ids_list = [4711,71,4715,4718,79,66,67,4716,4717]
 for ids in ids_list:
     replaced_content = ""
     filePath = 'provinces/' + str(ids) + ' -*.txt'
     fileName = glob.glob(filePath)
     if len(fileName) > 0:
         file = open(fileName[0], "r")
-        print("Changing file: " + fileName[0])
+        print("Looking at file: " + fileName[0])
         for line in file:
             line = line.strip()
 
-            if "owner = Z03" in line:
-                new_line = "controller = UBL"
-            elif "add_core = Z03" in line:
-                new_line = "add_core = UBL"
+            if "owner = Z02" in line:
+                new_line = "owner = FKN"
+            elif "add_core = Z02" in line:
+                new_line = "add_core = FKN"
+            elif "controller = Z02" in line:
+                new_line = "controller = FKN"
             else:
                 new_line = line
 
